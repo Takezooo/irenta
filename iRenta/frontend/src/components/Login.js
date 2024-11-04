0f5133f85435c2e95bb1b555acdd05393fe4e05f
Pat Castro <josuahcastro1@gmail.com>
2024-11-05 04:55:37 +0800

    Login Page Frontend w Tailwind: Done

--- iRenta/frontend/src/components/Login.js
+++ iRenta/frontend/src/components/Login.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
@@ -24,25 +25,31 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Login</button>
-            </form>
+        <div class="h-screen bg-gray-100 flex justify-center items-center flex-col font-sans">
+            <div class=" rounded-[10px] w-[80%] sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 2xl:w-1/6 h-fit text-center p-[24px] bg-gray-100 text-black shadow-lg border border-gray-400">
+                <h2 class="font-extrabold text-2xl text-blue-800 mb-1">LOGIN</h2>
+                <p class="text-xs mb-[30px]">Welcome to iRenta!</p>
+                <form onSubmit={handleSubmit} class="gap-4 flex justify-center items-center flex-col">
+                    <input class="w-[100%] px-[20px] py-[10px] rounded-[10px]"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                    <input class="w-[100%] px-[20px] py-[10px] rounded-[10px] mb-1"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <button type="submit" class="my-[10px] w-[100%] px-[20px] py-[10px] rounded-[10px] bg-blue-800 text-white hover:bg-blue-600 transition ease-in duration-300">Log in</button>
+                </form>
+            </div>
+            <Link to="/register">
+                <h3 class="mt-[10px] text-sm">New to iRenta? <span class="text-blue-600 hover:underline font-bold">Register</span></h3>
+            </Link>
         </div>
     );
 };

2e85796128608615bb0deddbebcb384ff6d80900
v1nss <vincemarc.mr@gmail.com>
2024-12-25 05:25:58 +0800

    added trim:true in users model

--- iRenta/backend/src/users/users.model.js
+++ iRenta/backend/src/users/users.model.js
@@ -2,28 +2,28 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   credentials: {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, trim: true }, // Trim whitespaces
     password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, trim: true }, // Trim whitespaces
   },
   info: {
-    firstName: { type: String, required: true },
-    middleName: { type: String, required: false },
-    lastName: { type: String, required: true },
-    birthDate: { type: Date, required: true},
+    firstName: { type: String, required: true, trim: true }, // Trim whitespaces
+    middleName: { type: String, required: false, trim: true }, // Trim whitespaces
+    lastName: { type: String, required: true, trim: true }, // Trim whitespaces
+    birthDate: { type: Date, required: true },
     gender: { type: String, required: true },
     phoneNumber: { type: Number, required: true },
     profile: {
-        id: { type: String },
-        name: { type: String },
-        link: { type: String},
+      id: { type: String },
+      name: { type: String, trim: true }, // Trim whitespaces
+      link: { type: String, trim: true }, // Trim whitespaces
     },
     userType: { type: String, enum: ["Seeker", "Owner"], required: true },
     address: {
-      houseNumber: { type: String },
-      street: { type: String },
-      city: { type: String },
-      zip: { type: String },
+      houseNumber: { type: String, trim: true }, // Trim whitespaces
+      street: { type: String, trim: true }, // Trim whitespaces
+      city: { type: String, trim: true }, // Trim whitespaces
+      zip: { type: String, trim: true }, // Trim whitespaces
     },
   },
   conversations: [
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
       ref: "Chat",
     },
   ],
-//   listings: [{ type: Types.ObjectId, ref: 'listings' }]
+  // listings: [{ type: Types.ObjectId, ref: 'listings' }]
 });
 
 const User = mongoose.model("User", userSchema);

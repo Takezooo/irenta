f55014a66467bf2f6562e2a961a55af461e5367e
Dave Jornales <plusultrastral@gmail.com>
2025-01-21 21:30:33 +0800

    server.js update and SocketIO.js

--- iRenta/backend/server.js
+++ iRenta/backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app); // Create the HTTP server
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: "https://irenta-delta.vercel.app/",
+    origin: "https://irenta-delta.vercel.app",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
 })); 

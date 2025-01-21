07e036bbf16929e443f108cc6646ccef6772a38d
Dave Jornales <plusultrastral@gmail.com>
2025-01-21 21:51:51 +0800

    server update

--- iRenta/backend/global/config/DB.js
+++ iRenta/backend/global/config/DB.js
@@ -1,16 +1,21 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI); 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, // Ensures SSL is enabled
+      sslValidate: false, // Skip SSL certificate validation (optional)
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;

e1cca90e55ddcecdd2f2189d20f16901538cb3bf
Dave Jornales <plusultrastral@gmail.com>
2025-03-30 03:06:52 +0800

    changed connection logic

--- iRenta/backend/global/config/DB.js
+++ iRenta/backend/global/config/DB.js
@@ -1,19 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       ssl: true, // Ensures SSL is enabled
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true, // Ensures SSL is enabled
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Exit with failure
   }
 };
 
